5cbd74b95a9865b2710f39fb2fc11fa24ab0ed2d
tolulope okeowo <lawalmichael35@yahoo.com>
2025-02-15 18:35:41 +0000

    Week02

---
+++ Week02/bank.py
@@ -0,0 +1,12 @@
+#Bank
+#This program should prompt users and read in two money amounts
+#author: Michael Lawal
+
+prompt1 = 'Enter First Amount (in cents):'
+Amount1= int(input(prompt1))
+prompt2 = 'Enter Second Amount (in cents):'
+Amount2= int(input(prompt2))
+Total_amount = Amount1 + Amount2
+Euros = Total_amount //100
+Cents = Total_amount %100
+print(f"Total amount is €{Euros}.{Cents:02d}")
